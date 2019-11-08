1db07a16dea8e4b46f3752aef7a6615a4735797a
jonathan <jonathan.fullola@enti.cat>
2019-11-08 09:49:24 +0100

    fragment + firebase

--- app/src/main/java/com/jonathanfullola/gamecompanion/MainActivity.kt
+++ app/src/main/java/com/jonathanfullola/gamecompanion/MainActivity.kt
@@ -2,11 +2,60 @@ package com.jonathanfullola.gamecompanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        bottomNavigationView.setOnNavigationItemReselectedListener { menuItem ->
+            //switch menu id
+            when(menuItem.itemId){
+                R.id.guide -> {
+                    //create fragment
+                    var guideFragment = GuideFragment()
+                    //add fragment to fragment container
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.add(fragmentContainer.id, guideFragment)
+                    fragmentTransaction.commit()
+
+                }
+
+                R.id.chat -> {
+                    //create fragment
+                    var chatFragment = ChatFragment()
+                    //add fragment to fragment container
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.add(fragmentContainer.id, chatFragment)
+                    fragmentTransaction.commit()
+
+                }
+
+                R.id.profile -> {
+                    //create fragment
+                    var profileFragment = ProfileFragment()
+                    //add fragment to fragment container
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.add(fragmentContainer.id, profileFragment)
+                    fragmentTransaction.commit()
+                }
+
+            }
+
+            true
+        }
+
+
+
+
+
+
     }
 }
