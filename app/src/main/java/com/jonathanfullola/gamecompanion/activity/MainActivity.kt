95761f230a09a7f3bca863fe9b7d132da7117898
jonathan <jonathan.fullola@enti.cat>
2019-11-22 11:11:21 +0100

    admob + analytics

--- app/src/main/java/com/jonathanfullola/gamecompanion/activity/MainActivity.kt
+++ app/src/main/java/com/jonathanfullola/gamecompanion/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.jonathanfullola.gamecompanion.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.jonathanfullola.gamecompanion.R
 import com.jonathanfullola.gamecompanion.fragment.ChatFragment
 import com.jonathanfullola.gamecompanion.fragment.GuideFragment
@@ -14,11 +17,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MobileAds.initialize(this){}
+        val adRequest = AdRequest.Builder().build()
+        bannerAdView.loadAd(adRequest)
 
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             //switch menu id
             when(menuItem.itemId){
                 R.id.guide -> {
+                    FirebaseAnalytics.getInstance(this).logEvent("Guide_Tab_Click",null)
                     //create fragment
                     var guideFragment = GuideFragment()
                     //add fragment to fragment container
@@ -32,6 +39,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.chat -> {
+
+                    // Obtain the FirebaseAnalytics instance.
+                    FirebaseAnalytics.getInstance(this).logEvent("Chat_Tab_Click",null)
                     //create fragment
                     var chatFragment = ChatFragment()
                     //add fragment to fragment container
@@ -44,6 +54,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.profile -> {
+                    FirebaseAnalytics.getInstance(this).logEvent("Profile_Tab_Click",null)
                     //create fragment
                     var profileFragment = ProfileFragment()
                     //add fragment to fragment container
