0ca2084ab81ff6b6b62c73f9aec4922cd3a445e3
jonathan <jonathan.fullola@enti.cat>
2019-11-29 11:21:14 +0100

    listadapter

--- app/src/main/java/com/jonathanfullola/gamecompanion/util/GuideListAdapter.kt
+++ app/src/main/java/com/jonathanfullola/gamecompanion/util/GuideListAdapter.kt
@@ -1,2 +1,20 @@
 package com.jonathanfullola.gamecompanion.util
 
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.jonathanfullola.gamecompanion.model.GuideModel
+import kotlinx.android.synthetic.main.item_guide.view.*
+
+
+class GuideListAdapter : RecyclerView.Adapter<GuideListAdapter.ViewHolder>(){
+
+    var elements = ArrayList<GuideModel>()
+
+    //TODO create view
+    
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val title: TextView = itemView.title
+}
+}
